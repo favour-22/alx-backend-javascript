fdcd8be351e1e83c92e4b7c88fc8a0c0e27c85c4
Mogbonjubola Favour <105045421+favour-22@users.noreply.github.com>
2023-05-17 11:45:38 +0100

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,15 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
-if (process.stdin.isTTY) {
-  process.stdin.on('namex', (namex) => {
-    process.stdout.write(`Your name is: ${namex.toString()}`);
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input !== null) {
+    const name = input.trim();
+    console.log(`Your name is: ${name}`);
     process.exit();
-  });
-} else {
-  process.stdin.on('namex', (namex) => {
-    process.stdout.write(`Your name is: ${namex.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+  }
+});
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
