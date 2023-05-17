e47491a4d76005005d441e485ffb6723c2b34249
Mogbonjubola Favour <105045421+favour-22@users.noreply.github.com>
2023-05-17 11:20:49 +0100

    -stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,14 @@
-process.stdin.setEncoding('utf8');
-
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
+
   if (input !== null) {
     const name = input.trim();
-    console.log(`Your name is: ${name}`);
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
 process.stdin.on('end', () => {
-  console.log('\nThis important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
