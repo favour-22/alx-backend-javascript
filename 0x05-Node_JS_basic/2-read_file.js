576cbcce1aead8cb00f3feccf4a538180522e005
Mogbonjubola Favour <105045421+favour-22@users.noreply.github.com>
2023-05-17 14:23:01 +0100

    read file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,38 +2,20 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter((line) => line); // Filter out empty lines
-
-    // Get the headers and remove the first column (ID)
-    const headers = lines[0].split(',').slice(1);
-
-    // Count the number of students in each field
-    const counts = {};
-    for (const header of headers) {
-      counts[header] = 0;
-    }
-    for (const line of lines.slice(1)) {
-      const fields = line.split(',');
-      for (let i = 1; i < fields.length; i++) {
-        const field = headers[i - 1];
-        if (fields[i].trim()) {
-          counts[field]++;
-        }
-      }
-    }
-
-    // Log the results
-    console.log(`Number of students: ${lines.length - 1}`);
-    for (const [field, count] of Object.entries(counts)) {
-      const list = lines.slice(1)
-        .filter((line) => line.split(',')[headers.indexOf(field) + 1].trim())
-        .map((line) => line.split(',')[0])
-        .join(', ');
-      console.log(`Number of students in ${field}: ${count}. List: ${list}`);
+    let data = fs.readFileSync(path, 'utf8').toString().split('\n');
+    data = data.slice(1, data.length - 1);
+    console.log(`Number of students: ${data.length}`);
+    const obj = {};
+    data.forEach((el) => {
+      const student = el.split(',');
+      if (!obj[student[3]]) obj[student[3]] = [];
+      obj[student[3]].push(student[0]);
+    });
+    for (const cls in obj) {
+      if (cls) console.log(`Number of students in ${cls}: ${obj[cls].length}. List: ${obj[cls].join(', ')}`);
     }
   } catch (err) {
-    console.error(`Cannot load the database: ${err}`);
+    throw new Error('Cannot load the database');
   }
 }
 
